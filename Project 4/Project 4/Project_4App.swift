060b3daf6cf338fa28ed7d915b79ed7862770c85
Evan <evan.sherfey@stu.barren.kyschools.us>
2024-10-22 08:28:28 -0500

    Project finished

---
+++ Project 4/Project 4/Project_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_4App.swift
+//  Project 4
+//
+//  Created by Student on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
